91f93123995949e7f52952256e94a125b50fd519
Shashank Mistry <shashankmistry30@gmail.com>
2022-03-19 19:49:51 +0530

    commit

---
+++ app/src/main/java/com/shashank/spendistrybusiness/Models/CreateInvoice/OTP.java
@@ -0,0 +1,33 @@
+package com.shashank.spendistrybusiness.Models.CreateInvoice;
+
+public class OTP {
+    private int otp;
+    private String email;
+    private String message;
+    private String mobile;
+
+    public OTP(String email,int otp) {
+        this.otp = otp;
+        this.email = email;
+    }
+
+    public OTP(String email) {
+        this.email = email;
+    }
+
+    public int getOtp() {
+        return otp;
+    }
+
+    public void setOtp(int otp) {
+        this.otp = otp;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
